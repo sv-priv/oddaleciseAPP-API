997cb928152b5dab4ece6982b89c78dbe7538bcc
StefanVelkoski <velkoskis@gmail.com>
2020-12-20 20:07:49 +0100

    cors

--- src/index.js
+++ src/index.js
@@ -25,6 +25,14 @@ app.use(express.json())
 //     })
 //     res.send("test");
 // })
+app.use(function(req,res,next){
+    req.header("Access-Control-Allow-Origin", "*");
+    req.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    req.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");    next();
+});
 
 app.get('/',(req,res)=>
 {
